c48156938948b52b5113e11f1c9190fbdd1221a3
Misiu <michel.wojciech@gmail.com>
2021-06-10 21:55:38 +0200

    create controller myapp only name of function

---
+++ nodejs-express-sequelize-postgresql/app/controllers/myapp.controller.js
@@ -0,0 +1,31 @@
+const db = require("../models");
+const MyApp = db.myapp;
+const Op = db.Sequelize.Op;
+
+exports.create = (req, res) => {
+    
+};
+
+exports.findAll = (req, res) => {
+    
+};
+
+exports.findOne = (req, res) => {
+    
+};
+
+exports.update = (req, res) => {
+    
+};
+
+exports.delete = (req, res) => {
+    
+};
+
+exports.deleteAll = (req, res) => {
+    
+};
+
+exports.findAllPublished = (req, res) => {
+    
+};
