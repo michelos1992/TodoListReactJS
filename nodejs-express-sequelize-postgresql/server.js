43183ebe24b4597ce2c22d88e02224961075114a
Misiu <michel.wojciech@gmail.com>
2021-06-10 21:21:20 +0200

    refactor server js

--- nodejs-express-sequelize-postgresql/server.js
+++ nodejs-express-sequelize-postgresql/server.js
@@ -5,14 +5,14 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-    origin: "http://localhost:8081";
+    origin: "http://localhost:8081"
 };
 
 app.use(cors(corsOptions));
 
-app.use(bodyParser.json());
+app.use(express.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to myApp." });
