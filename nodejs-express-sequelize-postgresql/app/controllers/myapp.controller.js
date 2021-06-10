3ee7f579f6617b91596fbd0812f8557cc0426dc7
Misiu <michel.wojciech@gmail.com>
2021-06-10 22:05:39 +0200

    create function code for creating data

--- nodejs-express-sequelize-postgresql/app/controllers/myapp.controller.js
+++ nodejs-express-sequelize-postgresql/app/controllers/myapp.controller.js
@@ -3,7 +3,27 @@ const MyApp = db.myapp;
 const Op = db.Sequelize.Op;
 
 exports.create = (req, res) => {
-    
+    if(!req.body.title) {
+        res.status(400).send({
+            message: "Content cant be empty!"
+        });
+        return;
+    }
+
+    const myapp = {
+        title: req.body.title,
+        description: req.body.description,
+        published: req.body.published ? req.body.published : false
+    };
+
+    MyApp.create(myapp).then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message:
+                err.message || "Some error occurred while creating the my app."
+        });
+    });
 };
 
 exports.findAll = (req, res) => {
