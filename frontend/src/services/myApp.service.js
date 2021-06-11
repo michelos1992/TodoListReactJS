3ad05d412052c411a65c7271dd63bc36c80463c1
Michael <michel.wojciech@gmail.com>
2021-06-11 19:29:29 +0200

    add data service

---
+++ frontend/src/services/myApp.service.js
@@ -0,0 +1,27 @@
+import http from '../http-common';
+
+class MyAppDataService {
+  getAll() {
+    return http.get("/myapps");
+  }
+  get(id) {
+    return http.get(`/myapps/${id}`);
+  }
+  create(data) {
+    return http.post("/myapps", data);
+  }
+  update(id, data) {
+    return http.put(`/myapps/${id}`, data);
+  }
+  delete(id) {
+    return http.delete(`/myapps/${id}`);
+  }
+  deleteAll() {
+    return http.delete(`/myapps`);
+  }
+  findByTitle(title) {
+    return http.get(`/myapps?title=${title}`);
+  }
+}
+
+export default new MyAppDataService();
